8a6b8c61c1544d11cc75d5616429b9befa0d4357
Nu Chaimongkon <anc.chaimongkon@gmail.com>
2019-08-14 11:59:54 +0700

    add data

--- js/core.js
+++ js/core.js
@@ -1,3 +1,13 @@
+var elementPosition = $("#numcat").offset();
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > elementPosition.top) {
+    $("#numcat").addClass("sticky-class");
+  } else {
+    $("#numcat").removeClass("sticky-class");
+  }
+});
+
 function readURL(input) {
   if (input.files && input.files[0]) {
     var reader = new FileReader();
@@ -14,6 +24,6 @@ $("#input-file").change(function() {
   readURL(this);
 });
 
-$("#img-file").click(function(){
-    $("#input-file").click();
-})
+$("#img-file").click(function() {
+  $("#input-file").click();
+});
